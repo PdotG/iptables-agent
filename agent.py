f645d6b5e21e4145639556bbf0a275ad8bdcca77
pablo <pablo@pablo.pablo>
2024-11-20 17:32:05 +0100

    added env variable to model name, also code has been refactorized

--- agent.py
+++ agent.py
@@ -11,13 +11,22 @@ from langchain.tools import Tool
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-# Load the OpenAI API key from the .env file
-load_dotenv()
-OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 
-# Logging configuration
-logging.basicConfig(filename="iptables_changes.log", level=logging.INFO,
+def basic_config(): 
+    # Load the OpenAI API key from the .env file
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    model_name = os.getenv("MODEL_NAME")
+
+    # Set the default model name
+    if(model_name is None):
+        model_name = "gpt-4o-mini"
+
+    # Logging configuration
+    logging.basicConfig(filename="iptables_changes.log", level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s")
+    
+    return api_key, model_name
 
 def list_rules(*args) -> str:
     """
@@ -49,53 +58,63 @@ def modify_rule(rule: str) -> str:
     except subprocess.CalledProcessError as e:
         return f"Error modifying the rule: {e}"
 
-# Tools
-list_rules_tool = Tool(name="list_rules", func=list_rules, description="Lists all the current rules in iptables.")
-modify_rule_tool = Tool(name="modify_rule", func=modify_rule, description="Adds or deletes a rule in iptables.")
-
-# Set up OpenAI API model
-llm = ChatOpenAI(api_key=str(OPEN_API_KEY), model="gpt-4o-mini", temperature=0.0)
-
-tools = [list_rules_tool, modify_rule_tool]
-
-# Prompt to include user's rules and give instructions to the agent
-prompt_template = """
-You are an expert security system that can interact with iptables to manage firewall rules. The user will provide a description of the task they want to perform, such as 'Allow HTTP traffic' or 'Delete a specific rule', and you need to generate the corresponding iptables rule(s).
-Also, the user may ask you for security recommendations, and you should provide them with the best practices for iptables.
-
-The current iptables rules are as follows:
-{current_rules}
-
-Your task:
-1. Understand the user's request, you will receive the current iptables list of the user.
-2. If the user wants to allow or block traffic, generate the appropriate iptables rule.
-3. It's important that you give the output in the same order as the user says.
-4. Use the provided tools to add or remove rules from iptables, you MUST include the whole command.
-5. Provide feedback to the user about what was done.
-
-The tools you have access to:
-- list_rules: List all current iptables rules.
-- modify_rule: Modify (add or delete) a rule in iptables.
-
-{input}
-
-{agent_scratchpad}
-"""
 
 
-prompt = PromptTemplate(input_variables=["input", "agent_scratchpad", "current_rules"], template=prompt_template)
+
+def agent_setup(api_key, model_name):
+
+    # Tools
+    list_rules_tool = Tool(name="list_rules", func=list_rules, description="Lists all the current rules in iptables.")
+    modify_rule_tool = Tool(name="modify_rule", func=modify_rule, description="Adds or deletes a rule in iptables.")
+
+    # Set up OpenAI API model
+    llm = ChatOpenAI(api_key=str(api_key), model=str(model_name), temperature=0.0)
+
+    tools = [list_rules_tool, modify_rule_tool]
+
+    # Prompt to include user's rules and give instructions to the agent
+    prompt_template = """
+    You are an expert security system that can interact with iptables to manage firewall rules. The user will provide a description of the task they want to perform, such as 'Allow HTTP traffic' or 'Delete a specific rule', and you need to generate the corresponding iptables rule(s).
+    Also, the user may ask you for security recommendations, and you should provide them with the best practices for iptables.
+
+    The current iptables rules are as follows:
+    {current_rules}
+
+    Your task:
+    1. Understand the user's request, you will receive the current iptables list of the user.
+    2. If the user wants to allow or block traffic, generate the appropriate iptables rule.
+    3. It's important that you give the output in the same order as the user says.
+    4. Use the provided tools to add or remove rules from iptables, you MUST include the whole command.
+    5. Provide feedback to the user about what was done.
+
+    The tools you have access to:
+    - list_rules: List all current iptables rules.
+    - modify_rule: Modify (add or delete) a rule in iptables.
+
+    {input}
+
+    {agent_scratchpad}
+    """
 
 
-agent = create_openai_functions_agent(llm, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    prompt = PromptTemplate(input_variables=["input", "agent_scratchpad", "current_rules"], template=prompt_template)
+
+
+    agent = create_openai_functions_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    return agent_executor
 
 def signal_handler(sig, frame):
     print('\nGracefully shutdown...\n')
     sys.exit(0)
 
+
 if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, signal_handler)
+    api_key, model_name = basic_config()
+    agent_executor = agent_setup(api_key, model_name)  # Set up the agent
+
     while True:
         user_input = input("Describe the action you want to perform (e.g., 'Allow HTTP traffic', 'Delete rule X'): ")
 
